9e0db4be14b10243463829f46953ea330009a375
Ismael Velten <ismael.velten@paybyrd.com>
2023-11-22 09:08:38 -0300

    Initial commit

---
+++ src/Main.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+import GHC.Generics
+import Data.Aeson
+import Data.Aeson.Casing
+import Data.Map as Map hiding (foldr)
+import Data.ByteString.Lazy.Char8 hiding (foldr, putStrLn)
+import Prelude hiding (readFile, writeFile)
+import System.IO hiding (readFile, writeFile)
+
+data AzureConfig = AzureConfig { name  :: String
+                               , value :: String
+                               , slotSetting :: Bool
+                               } deriving (Eq, Show, Generic)
+
+instance FromJSON AzureConfig where
+  parseJSON = genericParseJSON $ aesonDrop 0 camelCase
+
+data LocalSettings = LocalSettings { isEncrypted :: Bool
+                                   , values      :: Map String String
+                                   } deriving (Eq, Show, Generic)
+
+instance ToJSON LocalSettings where
+  toJSON = genericToJSON $ aesonDrop 0 pascalCase
+
+importSetting :: AzureConfig -> Map String String -> Map String String
+importSetting cfg = Map.insert (name cfg) (value cfg)
+
+convertSettings :: [AzureConfig] -> LocalSettings
+convertSettings cfgs =
+  LocalSettings { isEncrypted = False
+                , values      = foldr importSetting Map.empty cfgs }
+
+main :: IO ()
+main = do
+  hSetBuffering stdout NoBuffering
+  hSetBuffering stdin NoBuffering
+  putStrLn "Reading input file."
+  input <- readFile "input.json"
+  let azureConfig = eitherDecode input :: Either String [AzureConfig]
+  let localSettings = case azureConfig of
+        Left err   -> error err
+        Right cfgs -> convertSettings cfgs
+  let output = encode localSettings
+  putStrLn "Writing output file."
+  writeFile "local.settings.json" output
+  putStrLn "Done."
